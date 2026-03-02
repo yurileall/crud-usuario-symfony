40074f977c12fa3e4135246dfaffec5db3c87e43
yuri leal <yurileal111@outlook.com>
2026-03-02 17:14:47 -0300

    feat: criar entity com get e set.

---
+++ src/Entity/User.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nome = null;
+
+    #[ORM\Column(length: 150, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $senha = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $flgAtivo = true;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getSenha(): ?string
+    {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha): self
+    {
+        $this->senha = $senha;
+        return $this;
+    }
+
+    public function isFlgAtivo(): bool
+    {
+        return $this->flgAtivo;
+    }
+
+    public function setFlgAtivo(bool $flgAtivo): self
+    {
+        $this->flgAtivo = $flgAtivo;
+        return $this;
+    }
+}

44627e3c45cd2aaf264fb8691c1cc179ee100e05
yuri leal <yurileal111@outlook.com>
2026-03-24 07:21:24 -0300

    feat: criar entity Pet.

---
+++ src/Entity/Pet.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PetRepository::class)]
+class Pet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $id = null;
+
+    #[ORM\Column(type: "string", length: 100)]
+    private string $nome;
+
+    #[ORM\Column(type: "date")]
+    private \DateTimeInterface $data_nascimento;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getDataNascimento(): \DateTimeInterface
+    {
+        return $this->data_nascimento;
+    }
+
+    public function setDataNascimento(\DateTimeInterface $data_nascimento): self
+    {
+        $this->data_nascimento = $data_nascimento;
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+        return $this;
+    }
+}
